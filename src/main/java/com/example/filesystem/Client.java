46f8e7184a44cfffc26e7c16ec62547de2b392e1
rachel-lly <3556753750@qq.com>
2021-06-10 22:44:56 +0800

    UserLogin

---
+++ src/main/java/com/example/filesystem/Client.java
@@ -0,0 +1,97 @@
+package com.example.filesystem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+public class Client {
+
+
+    public static String ADDRESS_DEFAULT = "127.0.0.1";
+
+    public static int PORT_DEFAULT = 9023;
+
+
+    public static void main(String[] args) {
+
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            Selector selector = Selector.open();
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            socketChannel.connect(new InetSocketAddress(ADDRESS_DEFAULT,PORT_DEFAULT));
+
+            while (true){
+                selector.select();
+                Set<SelectionKey> keySet = selector.selectedKeys();
+
+                for (SelectionKey selectionKey : keySet) {
+                    if (selectionKey.isConnectable()) {
+                        //表明已经与服务器建立好连接
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        //连接是否正在进行
+                        if (client.isConnectionPending()) {
+                            client.finishConnect();
+                            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+//                            byteBuffer.put((LocalDateTime.now() + "连接成功").getBytes());
+//                            byteBuffer.flip();
+//                            client.write(byteBuffer);
+                            //开启线程池,用于用户的输入（阻塞的）
+                            ExecutorService executorService = newSingleThreadExecutor(Executors.defaultThreadFactory());
+                            executorService.submit(() ->{
+                                while (true) {
+                                    try {
+                                        if (UserLogin.loginMap.isEmpty()) {
+                                            //说明刚刚进来，需要登录，调用登录方法
+                                            User user = UserLogin.userLogin(byteBuffer, client);
+                                            UserLogin.loginMap.put(client, user);
+                                        }
+                                        else {
+                                            byteBuffer.clear();
+                                            InputStreamReader input = new InputStreamReader(System.in);
+                                            BufferedReader reader = new BufferedReader(input);
+                                            String sendMessage = reader.readLine();
+                                            byteBuffer.put(sendMessage.getBytes());
+                                            byteBuffer.flip();
+                                            client.write(byteBuffer);
+                                        }
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                        client.register(selector, SelectionKey.OP_READ);
+                    } else if (selectionKey.isReadable()) {
+                        SocketChannel client = (SocketChannel) selectionKey.channel();
+                        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                        int count = client.read(readBuffer);
+                        if (count > 0) {
+                            String receiveMessage = new String(readBuffer.array(), 0, count);
+                            //对收到的消息进行处理
+
+                            // TODO: 2021/6/10 MessageUtil
+                           // MessageUtil.clientMessage(receiveMessage, client);
+                        }
+                    }
+                }
+                keySet.clear();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/filesystem/UserLogin.java
@@ -0,0 +1,75 @@
+package com.example.filesystem;
+
+import com.example.filesystem.util.JudgeUtil;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+@Service
+public class UserLogin {
+
+    @Resource
+    private UserMapper mapper;
+
+    public static Map<SocketChannel, User> loginMap = new HashMap<>();
+
+    public static User userLogin(ByteBuffer byteBuffer, SocketChannel client){
+        byteBuffer.clear();
+        String name,password;
+        User user = new User();
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("name:");
+            name = scanner.nextLine();
+            System.out.print("password:");
+            password = scanner.nextLine();
+            if (JudgeUtil.isStringEmpty(name) || JudgeUtil.isStringEmpty(password)) {
+                System.out.println("请填写完整账号信息");
+            }
+            else {
+                break;
+            }
+        }
+        user.setName(name);
+        user.setPassword(password);
+        user.setId(1);
+        String message = "username:" + name + "," + "password:" + password;
+        byteBuffer.put(message.getBytes());
+        byteBuffer.flip();
+        try {
+            client.write(byteBuffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO异常");
+        }
+        return user;
+    }
+
+
+
+    public User login(String name,String password){
+        if(JudgeUtil.isStringEmpty(name)||JudgeUtil.isStringEmpty(password)){
+            System.out.println("enter empty");
+            return null;
+        }
+
+        User user = mapper.getUser(name,password);
+
+        if(user!=null){
+            System.out.println("Login successfully!");
+            return user;
+        }else{
+            System.out.println("Couldn't find this account!");
+            return null;
+        }
+
+    }
+
+
+}
